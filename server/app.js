f629eaaa2570c3ea3fc206a4df41060bcfa06a77
headStyleColorRed <headStyleColorRed@gmail.com>
2021-11-13 12:56:42 +0100

    added prompts

---
+++ server/app.js
@@ -0,0 +1,49 @@
+const prompts = require('prompts');
+
+// Modules
+const fileCreator = require("./fileCreator.js");
+
+(async () => {
+  const response = await prompts([
+    {
+      type: 'text',
+      name: 'projectName',
+      message: `What's your project name?`
+    },
+    {
+      type: 'number',
+      name: 'port',
+      message: 'On which port do you want the server to run?',
+      initial: 8080,
+      style: 'default'
+    },
+    {
+      type: 'multiselect',
+      name: 'options',
+      message: 'Choose configuration',
+      instructions: false,
+      choices: [
+        { title: 'Cors', value: "cors", selected: true },
+        { title: 'BodyParser', value: 'bodyParser', selected: true },
+        { title: 'Dot files', value: 'dotfiles' },
+        { title: 'Mongoose', value: 'mongoose' },
+      ],
+      hint: '- Space to select. Return to submit'
+    }
+  ]);
+
+  // Create user requirements object
+  serverRequirements = {
+    projectName: response.projectName,
+    port: response.port,
+    cors: response.options.includes("cors"),
+    bodyParser: response.options.includes("bodyParser"),
+    mongoose: response.options.includes("mongoose"),
+    getMethods: ['get-books'],
+    postMethods: ['add-book delete-book']
+  }
+  console.log(serverRequirements);
+
+  // Create project accordingly
+  fileCreator.createAppFile(serverRequirements)
+})();
